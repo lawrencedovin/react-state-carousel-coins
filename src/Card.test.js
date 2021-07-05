5a64c1832635c2f3253646c45b28740fda2d17c8
Lawrence Dovin <lawrencedovin@gmail.com>
2021-07-05 19:37:49 -0400

    Updated and added additional snapshot test to Card component

--- src/Card.test.js
+++ src/Card.test.js
@@ -6,8 +6,17 @@ it('should render Card component', () => {
     render(<Card />);
 })
 
+it('matches snapshot', () => {
+    const { asFragment } = render(<Card />);
+    expect(asFragment()).toMatchSnapshot();
+})
+
 it('should show props passed in', () => {
     const { getByText } = render(<Card caption="i'm a card" src="google.com" currNum={1} totalNum={3}/>)
-    const h4 = getByText("i'm a card");
-    expect(h4).toHaveClass("Card-title");
+    const caption = getByText("i'm a card");
+    const imgText = getByText("Image 1 of 3.");
+    expect(caption).toHaveClass("Card-title");
+    expect(caption).toBeInTheDocument();
+    expect(imgText).toHaveClass("Card-small");
+    expect(imgText).toBeInTheDocument();
 })
